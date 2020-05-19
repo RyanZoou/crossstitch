390720f65742441200bb7a2bec3239211e576725
RyanZou <ryanzoou@163.com>
2020-05-19 19:14:04 +0800

    修改和删除收货地址

--- app/Http/Controllers/UserAddressesController.php
+++ app/Http/Controllers/UserAddressesController.php
@@ -54,4 +54,58 @@ class UserAddressesController extends Controller
         return redirect()->route('user_addresses.index');
     }
 
+
+    /**
+     * update
+     *
+     * @param UserAddress $user_address
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
+    public function edit(UserAddress $user_address)
+    {
+        $this->authorize('own', $user_address);
+        return view('user_addresses.create_and_edit', ['address' => $user_address]);
+    }
+
+
+    /**
+     * save update
+     *
+     * @param UserAddress $user_address
+     * @param UserAddressRequest $request
+     * @return \Illuminate\Http\RedirectResponse
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
+    public function update(UserAddress $user_address, UserAddressRequest $request)
+    {
+        $this->authorize('own', $user_address);
+        $user_address->update($request->only([
+            'province',
+            'city',
+            'district',
+            'address',
+            'zip',
+            'contact_name',
+            'contact_phone',
+        ]));
+
+        return redirect()->route('user_addresses.index');
+    }
+
+
+    /**
+     * delete address
+     *
+     * @param UserAddress $user_address
+     * @return array
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
+    public function destroy(UserAddress $user_address)
+    {
+        $this->authorize('own', $user_address);
+        $user_address->delete();
+        return [];
+    }
+
 }
