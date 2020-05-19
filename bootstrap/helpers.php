4be8d3fdc5c200f940620c38b48f7c743ffb7767
RyanZou <ryanzoou@163.com>
2020-05-19 16:51:44 +0800

    基础布局

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -5,6 +5,7 @@
  */
 
 
-function test_helper() {
-    return 'OK';
+function route_class()
+{
+    return str_replace('.', '-', Route::currentRouteName());
 }
---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="footer">
+    <div class="container">
+        <p class="float-left">Hello CrossStitch!!!</p>
+        <p class="float-right"><a href="mailto:ryanzoou@gmail.com">联系我们</a></p>
+    </div>
+</footer>
