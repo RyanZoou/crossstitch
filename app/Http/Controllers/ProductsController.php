0a16a7da65a590766e200b4ec5a55b9f38351827
RyanZou <ryanzoou@163.com>
2020-05-20 10:40:51 +0800

    用户界面 - 商品详情页

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -57,4 +57,23 @@ class ProductsController extends Controller
         ]);
     }
 
+
+    /**
+     * 展示商品详情
+     *
+     * @param Product $product
+     * @param Request $request
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     * @throws \Exception
+     */
+    public function show(Product $product, Request $request)
+    {
+        // 判断商品是否已经上架，如果没有上架则抛出异常。
+        if (!$product->on_sale) {
+            throw new \Exception('商品未上架');
+        }
+
+        return view('products.show', ['product' => $product]);
+    }
+
 }
