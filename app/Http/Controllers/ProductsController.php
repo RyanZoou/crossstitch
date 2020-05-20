bb6db8b17701a201029a2412e32a4d6899a2791e
RyanZou <ryanzoou@163.com>
2020-05-20 10:22:29 +0800

    用户界面 - 商品列表

---
+++ app/Http/Controllers/ProductsController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Product;
+
+use Illuminate\Http\Request;
+
+class ProductsController extends Controller
+{
+
+    /**
+     * 获取商品列表
+     *
+     * @param Request $request
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function index(Request $request)
+    {
+        $products = Product::query()->where('on_sale', true)->paginate(16);
+
+        return view('products.index', ['products' => $products]);
+    }
+
+}
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Product extends Model
 {
@@ -24,6 +25,22 @@ class Product extends Model
     public function skus()
     {
         return $this->hasMany(ProductSku::class);
+    }
+
+
+    /**
+     * 商品的 image 字段保存的是图片的相对于目录 storage/app/public/ 的路径
+     * 需要转成绝对路径才能正常展示，我们可以给商品模型加一个访问器来输出绝对路径
+     *
+     * @return mixed
+     */
+    public function getImageUrlAttribute()
+    {
+        // 如果 image 字段本身就已经是完整的 url 就直接返回
+        if (Str::startsWith($this->attributes['image'], ['http://', 'https://'])) {
+            return $this->attributes['image'];
+        }
+        return \Storage::disk('public')->url($this->attributes['image']);
     }
 
 }
