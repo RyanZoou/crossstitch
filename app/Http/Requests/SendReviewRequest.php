f8297031c219484f35a3cabf855ae10f0c71733f
ZouShirong <v_zoushirong@baidu.com>
2020-05-23 13:06:29 +0800

    评价商品

---
+++ app/Http/Requests/SendReviewRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Validation\Rule;
+
+/**
+ * 评价表单验证
+ *
+ * Class SendReviewRequest
+ * @package App\Http\Requests
+ */
+class SendReviewRequest extends Request
+{
+    public function rules()
+    {
+        return [
+            'reviews'          => ['required', 'array'],
+            'reviews.*.id'     => [
+                'required',
+                Rule::exists('order_items', 'id')->where('order_id', $this->route('order')->id)
+            ],
+            'reviews.*.rating' => ['required', 'integer', 'between:1,5'],
+            'reviews.*.review' => ['required'],
+        ];
+    }
+
+
+    public function attributes()
+    {
+        return [
+            'reviews.*.rating' => '评分',
+            'reviews.*.review' => '评价',
+        ];
+    }
+
+}
